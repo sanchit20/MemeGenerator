a5d2f2cbad8c5edc70427076d5ead2aae62e2c8d
sanchit20 <44345426+sanchit20@users.noreply.github.com>
2020-10-09 14:07:42 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './Header'
+import './Memegen'
+import Header from './Header';
+import Memegen from './Memegen';
+
+function App() {
+  return (<div>
+    
+    <Header/>
+  
+    <Memegen/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header(){
+    return(
+        <header>
+            <img className="img"
+            src="https://www.pinclipart.com/picdir/middle/451-4511211_trollface-png-image-free-download-funny-face-black.png"
+            alt="Problem"
+            />
+            <p>MEME GENERATOR</p>
+        </header>
+    )
+}
+
+export default Header
